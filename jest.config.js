09f58bad9173443059bfa6dffb390dcf96d590ba
bryan <e9874687@gmail.com>
2026-06-23 22:31:19 +0800

    安裝設定 jest 測試環境

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	roots: ['<rootDir>'],
+	testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/$1',
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.json',
+			},
+		],
+	},
+}
+
+module.exports = config
